3b2d95548957213823ff67eb2341421ff510ec77
Tarek-Issaoui <tarek9.9issaoui@gmail.com>
2023-07-10 18:49:31 +0100

    learning spring boot

---
+++ src/main/java/com/issaoui/movies/Movie.java
@@ -0,0 +1,26 @@
+package com.issaoui.movies;
+// import com.issaoui.movies.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+@Document(collection="movies")
+@Data
+@AllArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
